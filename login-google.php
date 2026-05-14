80abc848389be7dde51cfed7499e666f6f3625e5
alideeb-coder <alideeb566056@gmail.com>
2026-05-14 23:12:17 +0300

    Update login-google.php

--- login-google.php
+++ login-google.php
@@ -6,7 +6,7 @@ $client = new Google_Client();
 $config = require 'config/google.php';
 $client->setClientId($config['google_client_id']);
 $client->setClientSecret($config['google_client_secret']);
-$client->setRedirectUri('http://localhost/chat-app/callback-google.php');
+$client->setRedirectUri('https://chat-app-vroj.onrender.com/callback-google.php');
 $client->addScope('email');
 $client->addScope('profile');
 
