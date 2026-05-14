2bc5d0980357763e33d4f99c226e192a410aafb0
alideeb-coder <alideeb566056@gmail.com>
2026-05-15 00:33:04 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -35,7 +35,8 @@ class Database
                 $options = [
                     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                    PDO::MYSQL_ATTR_SSL_CA       => '/etc/ssl/certs/ca-certificates.crt',
+                    PDO::MYSQL_ATTR_SSL_CA       => true,
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
                 ];
                 
                 $this->conn = new PDO($dsn, $this->user, $this->password, $options);
